062f5acf9c207abc83df5b2cf3b70e128011aa57
Dmytro Bogdanov <Dmytro.Bogdanov@harmonicinc.com>
2019-04-16 15:57:42 +0300

    Added btn, that can apply the number of the ticket the filtered list

--- popup.js
+++ popup.js
@@ -1,26 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const btn = document.getElementById('btn-set-enumerate');
-  btn.addEventListener('click', enumerateTickets);
+  const btnSetEnumeration = document.getElementById('btn-set-enumerate');
+  const btnApplyEnumeration = document.getElementById('btn-apply-enumeration');
+  const cbxAdvancedSettings = document.getElementById('cbx-advanced-settings');
 
-  const cbx = document.getElementById('cbx-advanced-settings');
-  cbx.addEventListener('change', (event) => {
+  btnSetEnumeration.addEventListener('click', enumerateTickets);
+
+  btnApplyEnumeration.addEventListener('click', applyTicketEnumeration);
+
+  cbxAdvancedSettings.addEventListener('change', (event) => {
     const settingsDiv = document.getElementsByClassName('advanced-settings');
     if (event.target.checked) {
       settingsDiv[0].classList.remove('hide');
     } else {
       settingsDiv[0].classList.add('hide');
     }
-  })
+  });
 });
 
 function enumerateTickets() {
   chrome.tabs.executeScript(
     null,
     {
-      code: "[].forEach.call(document.getElementsByClassName('js-key-link'), (el, i) => { el.innerText = `№: ${i+1} - ${el.innerText}` });"
+      code: "var list = [].map.call(document.getElementsByClassName('js-key-link'), (el, i) => el.innerText); [].forEach.call(document.getElementsByClassName('js-key-link'), (el, i) => { el.innerText = `№: ${i+1} - ${el.innerText}` });"
     }
   );
   window.close();
 }
 
+function applyTicketEnumeration() {
+  chrome.tabs.executeScript(
+    null,
+    {
+      code: "[].forEach.call(document.getElementsByClassName('js-key-link'), (el, i) => { el.innerText = `№: ${list.indexOf(el.innerText) + 1} - ${el.innerText}` });"
+    }
+  );
+  window.close();
+}
